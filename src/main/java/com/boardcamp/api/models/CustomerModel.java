b77cb6f85a6d5954a1e36c6f90f7163e58f0e813
jefti <jeftimeira@gmail.com>
2024-02-01 21:56:14 -0300

    Feat: Rental model

--- src/main/java/com/boardcamp/api/models/CustomerModel.java
+++ src/main/java/com/boardcamp/api/models/CustomerModel.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "customer")
+@Table(name = "customers")
 public class CustomerModel {
     
     @Id
---
+++ src/main/java/com/boardcamp/api/models/RentalModel.java
@@ -0,0 +1,49 @@
+package com.boardcamp.api.models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "rentals")
+public class RentalModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate rentDate;
+
+    @Column(nullable = false)
+    private int daysRented;
+
+    @Column(nullable = true)
+    private LocalDate returnDate;
+
+    @Column(nullable = false)
+    private int originalPrice;
+
+    @Column(nullable = false)
+    private int delayFee;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private GameModel game;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private CustomerModel customer;
+}
