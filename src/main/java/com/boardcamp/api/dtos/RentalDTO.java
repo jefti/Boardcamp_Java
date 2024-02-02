f9dfbfac77d9619f7d1fe2f376f77a7f4fabf261
jefti <jeftimeira@gmail.com>
2024-02-01 22:56:49 -0300

    Feat: Rental DTO

---
+++ src/main/java/com/boardcamp/api/dtos/RentalDTO.java
@@ -0,0 +1,20 @@
+package com.boardcamp.api.dtos;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class RentalDTO {
+    @NotNull(message = "Field customerId is mandatory.")
+    @Min(value = 1, message = "Customer id cannot be less then 1.")
+    private long customerId;
+
+    @NotNull(message = "Field gameId is mandatory.")
+    @Min(value = 1, message = "Game id cannot be less then 1.")
+    private long gameId;
+
+    @NotNull(message = "Field daysRented is mandatory.")
+    @Min(value = 1, message = "Days rented cannot be less then 1.")
+    private int daysRented;
+}
