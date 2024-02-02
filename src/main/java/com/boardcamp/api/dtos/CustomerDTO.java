6e973814483734502ca47edad60b78f8ecb4c2d2
jefti <jeftimeira@gmail.com>
2024-02-01 22:50:03 -0300

    Feat: Game DTO

---
+++ src/main/java/com/boardcamp/api/dtos/CustomerDTO.java
@@ -0,0 +1,13 @@
+package com.boardcamp.api.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public class CustomerDTO {
+    @NotBlank(message = "Field name is mandatory.")
+    private String name;
+
+    @NotBlank(message = "Field cpf is mandatory.")
+    @Pattern(regexp = "^\\d{11}$", message = "Field cpf must contain only numeric characters.")
+    private String cpf;
+}
