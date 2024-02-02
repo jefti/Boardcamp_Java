886f79684310db2902c5dfb919ac597797d91b04
jefti <jeftimeira@gmail.com>
2024-02-01 21:28:29 -0300

    Feat: Customer model

---
+++ src/main/java/com/boardcamp/api/models/CustomerModel.java
@@ -0,0 +1,30 @@
+package com.boardcamp.api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customer")
+public class CustomerModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true,length = 11)
+    private String cpf;
+
+}
